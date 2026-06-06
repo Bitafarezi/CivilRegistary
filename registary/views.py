6d9cef019924b0eed91d794e7ab6b5aae7cc081c
Bita <bitafarezi79@gmail.com>
2026-06-06 17:55:16 +0330

    Add home and register views

--- registary/views.py
+++ registary/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'templates/home.html')
+
+def register_view(request):
+    form = UserCreationForm()
+    return render(request, 'templates/register.html', {'form': form})
