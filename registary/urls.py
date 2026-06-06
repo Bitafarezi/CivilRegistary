a48d737daf5324472433b211026485a648b5c8c0
Bita <bitafarezi79@gmail.com>
2026-06-06 18:19:28 +0330

    Correct the templates path to be recognized

--- registary/urls.py
+++ registary/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home_view, register_view
 
 urlpatterns = [
-    path("home/", home_view, name="home"),
+    path("", home_view, name="home"),
     path("register/", register_view, name="register")
 ]
--- registary/views.py
+++ registary/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'templates/home.html')
+    return render(request, 'home.html')
 
 def register_view(request):
     form = UserCreationForm()
-    return render(request, 'templates/register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
