b710fcd13e8b21cd8c251987088271206dc75a39
Bita <bitafarezi79@gmail.com>
2026-06-07 10:09:45 +0330

    Add login and logout views and pages

--- registary/views.py
+++ registary/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
@@ -8,12 +10,19 @@ def home_view(request):
 
 def register_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         
         if form.is_valid():
             form.save()
-            return redirect('home')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account was created for {username}!")
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
         
     return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
