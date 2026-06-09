75c5d0a140dac068f5ed765f3db49e4a042f1028
Bita <bitafarezi79@gmail.com>
2026-06-09 17:38:06 +0330

    Design and add some features to admin panel

--- registary/admin.py
+++ registary/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import UserProfile, Citizen, GlobalSettings
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'daily_usage', 'daily_limit', 'monthly_usage', 'monthly_limit')
+    search_fields = ('user__username',)
+    ordering = ('first_name',)
+
+@admin.register(Citizen)
+class CitizenAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'national_id', 'phone_number')
+    search_fields = ('first_name', 'last_name', 'national_id', 'phone_number')
+    ordering = ('first_name',)
+
+@admin.register(GlobalSettings)
+class GlobalSettingsAdmin(admin.ModelAdmin):
+    list_display = ('default_daily_limit', 'default_monthly_limit')
