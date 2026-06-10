62de1ea60e395a3478cf4052b00744497a04c471
Bita <bitafarezi79@gmail.com>
2026-06-10 10:41:55 +0330

    Add signals to make sure when a user registers, profile will be created

--- registary/apps.py
+++ registary/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class RegistaryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'registary'
+
+    def ready(self):
+        import registary.signals
